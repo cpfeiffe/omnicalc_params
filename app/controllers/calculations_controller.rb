e93f41f32d151405efaa07434a1446800808dae5
cpfeiffe <cpfeiffe@users.noreply.github.com>
2017-04-20 16:48:34 -0500

    All the changes!

---
+++ app/controllers/calculations_controller.rb
@@ -0,0 +1,53 @@
+class CalculationsController < ApplicationController
+
+  def flex_square
+    @user_number = params["number"].to_f
+    @square = @user_number **2
+    render("calculations/flex_square.html.erb")
+  end
+
+  def flex_square_root
+    @user_number = params["number"].to_f
+    @squareroot = @user_number **0.5
+    render("calculations/flex_square_root.html.erb")
+  end
+
+  def flex_payment
+    @years = params["years"].to_f
+    @principal = params["principal"].to_f
+    @apr = params["apr"].to_f/100
+    @monthly_payment = (((@apr/100/12) * (@principal)) / (1- (1+@apr/100/12)**(@years*-12)))
+    render("calculations/flex_payment.html.erb")
+  end
+
+  def flex_random
+    @min = params["min"].to_f
+    @max = params["max"].to_f
+    @user_number = rand(@min..@max)
+    render("calculations/flex_random.html.erb")
+  end
+
+  def square_form
+    render("calculations/square_form.html.erb")
+  end
+
+  def square_results
+    @user_number = params[:user_number].to_f
+    @square = @user_number**2
+    render("calculations/square_results.html.erb")
+  end
+
+  def squareroot_form
+    render("calculations/squareroot_form.html.erb")
+  end
+
+  def squareroot_results
+    @user_number = params[:user_number].to_f
+    @squareroot = @user_number**0.5
+    render("calculations/squareroot_results.html.erb")
+  end
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get("/flexible/square/:number", {:controller => "calculations", :action => "flex_square"})
+  get("/flexible/squareroot/:number", {:controller => "calculations", :action => "flex_square_root"})
+  get("/flexible/payment/:apr/:years/:principal", {:controller => "calculations", :action => "flex_payment"})
+  get("/flexible/random/:min/:max", {:controller => "calculations", :action => "flex_random"})
+
+
+
+  get("/square/new", {:controller => "calculations", :action => "square_form"})
+  get("/square/results", {:controller => "calculations", :action => "square_results"})
+
+  
+  get("/squareroot/new", {:controller => "calculations", :action => "squareroot_form"})
+  get("/squareroot/results", {:controller => "calculations", :action => "squareroot_results"})
+
+
 end
